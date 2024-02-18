d9031c1689d94ed5e348536db312485d81d90076
Kolli Mohit Kumar <mohithkolli@gmail.com>
2024-02-18 16:08:28 +0530

    added class names

--- docs/index.js
+++ docs/index.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    {
-        setInterval(()=>{
-        document.getElementById("td1").textContent = Math.floor(Math.random()*201)-100;
-        document.getElementById("td2").textContent = Math.floor(Math.random()*201)-100;
-        document.getElementById("td3").textContent = Math.floor(Math.random()*201)-100;
-        document.getElementById("td4").textContent = Math.floor(Math.random()*201)-100;
-        },1000)
-    }
+    var cells=document.querySelectorAll(".value-column");
+    cells.forEach(cell => {
+        cell.textContent=Math.floor(Math.random()*201)-100;
+        
+    });
+
 })
