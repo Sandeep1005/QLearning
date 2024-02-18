1e403c41d91eca420c9705cd506c570b3c524d32
mohit12356 <85504379+mohit12356@users.noreply.github.com>
2024-02-18 18:45:58 +0530

    Mohit dev (#4)
    * created and added table
    
    * created and added table
    
    * changed index and js file names
    
    * added class names
    
    * changes commited

--- docs/index.js
+++ docs/index.js
@@ -8,13 +8,5 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 })
 
-    {
-        setInterval(()=>{
-        document.getElementById("td1").textContent = Math.floor(Math.random()*201)-100;
-        document.getElementById("td2").textContent = Math.floor(Math.random()*201)-100;
-        document.getElementById("td3").textContent = Math.floor(Math.random()*201)-100;
-        document.getElementById("td4").textContent = Math.floor(Math.random()*201)-100;
-        },1000)
-    }
-})
+   
 
